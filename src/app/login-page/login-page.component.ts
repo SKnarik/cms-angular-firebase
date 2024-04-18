9c8044697faf725c1a81a3db0c81c2d985b300de
SKnarik <knarik.soghoyan.fd@gmail.com>
2024-04-18 12:05:58 +0400

    Add Authorization with FireBase

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -1,14 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AfService } from '../providers/af.service';
+import { User } from '../providers/user';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.css'
 })
-export class LoginPageComponent {
+
+export class LoginPageComponent implements OnInit {
+  user: User;
+
   constructor(public AfService: AfService) { }
 
+  ngOnInit() {
+    this.AfService.user$.subscribe(user => this.user = user);
+  }
   login() {
     this.AfService.loginWithGoogle();
   }
