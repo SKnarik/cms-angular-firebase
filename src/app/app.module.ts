7d027f27aed39faa6055b2a4c879a39d78c0edae
SKnarik <knarik.soghoyan.fd@gmail.com>
2024-04-23 23:19:43 +0400

    Add front-page module and changed routing structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,9 @@ import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
 
 import { AppComponent } from './app.component';
 import { environment } from '../environments/emvironment';
-import { PagesListComponent } from './pages-list/pages-list.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AfService } from './providers/af.service';
-import { HomePageComponent } from './home-page/home-page.component';
 import { AdminGuard } from './guards/admin.guard';
 import { SubscriberGuard } from './guards/subscriber.guard';
 
@@ -23,9 +21,7 @@ import { PostsService } from './service/posts/posts.service';
 @NgModule({
   declarations: [
     AppComponent,
-    PagesListComponent,
     LoginPageComponent,
-    HomePageComponent
   ],
   imports: [
     BrowserModule,
